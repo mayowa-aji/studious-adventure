e25414204a7c2d3a2418eb7ed6d73da0236f7e0a
Mayowa Ajiboye <64916584+mayowa-aji@users.noreply.github.com>
2023-03-23 10:38:50 -0400

    finish code challenge

--- server/app.py
+++ server/app.py
@@ -18,25 +18,86 @@ db.init_app(app)
 def home():
     return ''
 
-@app.route('/heroes')
+@app.route('/heroes', methods = ['GET'])
 def heroes():
-    return ''
+    heroes_list = [hero.to_dict() for hero in Hero.query.all()]
+    return make_response(jsonify(heroes_list),200)
 
-@app.route('/heroes/<int:id>')
+@app.route('/heroes/<int:id>', methods=['GET'])
 def hero_by_id(id):
-    return ''
+    hero = Hero.query.filter(Hero.id == id).first()
 
-@app.route('/powers')
+    if not hero:
+        return make_response(jsonify({'error': 'Hero not found'}),404)
+
+    if request.method == 'GET':
+        return make_response(jsonify(hero.to_dict()),200)
+
+@app.route('/powers', methods=['GET'])
 def powers():
-    return ''
+    power_list = [power.to_dict() for power in Power.query.all()]
+    return make_response(jsonify(power_list),200)
 
-@app.route('/powers/<int:id>')
-def power_by_id(id):
-    return ''
 
-@app.route('/hero_powers')
+@app.route('/powers/<int:id>', methods=['GET', 'PATCH'])
+def powers_with_id(id):
+    power = Power.query.filter(Power.id == id).first()
+
+    if not power:
+        response_dict = { "error": "Power not found"}
+        response = make_response(
+            jsonify(response_dict),
+            404
+        )
+        return response
+
+    if request.method == 'GET':
+        response = make_response(jsonify(power.to_dict()), 200)
+        return response
+
+    elif request.method == 'PATCH':
+        request_json = request.get_json()
+
+        for key in request_json:
+            setattr(power, key, request_json[key])
+
+            db.session.add(power)
+            db.session.commit()
+
+            if not power:
+                response_dict = { "error": "Power not found"}
+                response = make_response(jsonify(response_dict),404)
+                return response
+
+        return make_response(jsonify(power.to_dict()), 200)
+
+@app.route('/hero_powers', methods = ['POST'])
 def hero_powers():
-    return ''
+    request_json = request.get_json()
+    new_hero_powers = HeroPower(
+        strength = request_json.get('strength'),
+        power_id = request_json.get('power_id'),
+        hero_id = request_json.get('hero_id')
+    )
+
+    db.session.add(new_hero_powers)
+    db.session.commit()
+
+    if not new_hero_powers:
+        response_dict = { "error": "Invalid input"}
+        response = make_response(
+            jsonify(response_dict),
+            404
+            )
+        return response
+
+    else:
+        hero = new_hero_powers.hero
+
+        response = make_response(
+            jsonify(hero.to_dict()),201)
+
+        return response
 
 
 if __name__ == '__main__':
